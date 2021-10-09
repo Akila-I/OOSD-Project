41291182936440d5b45cfb8a1562f62b752e4642
akila99g <indurangakga.19@uom.lk>
2021-10-09 09:01:24 +0530

    introducing dummy for pdo.php

---
+++ src/dummy_pdo.php
@@ -0,0 +1,14 @@
+<?php
+$pdo = new PDO('mysql:host=localhost;port=3306;dbname=Library', 'username', 'password');
+
+//this is a dummy file. Create your own pdo.php file in src folder
+//replacing username and password with you mysql username and password.
+//consider changing the port if your mysql is running on a different port.
+
+//DO NOT PUSH YOUR OWN PDO.PHP FILE TO GIT REPO. !!!
+
+
+//you also need to have a database named 'Library' with the schema tables.
+//use DB_init_SQL.sql file to get the sql commands to create that database and required tables
+
+//DO NOT MODIFY THIS FILE OR DB_INIT_SQL FILE !!!
