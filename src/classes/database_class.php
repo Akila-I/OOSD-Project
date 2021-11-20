38690e021d68a70c996fd131e1e7dc834999e6a4
akila99g <indurangakga.19@uom.lk>
2021-11-20 23:20:44 +0530

    HomePage modified

--- src/classes/database_class.php
+++ src/classes/database_class.php
@@ -57,7 +57,7 @@ class database{
 
         $statement = $this->pdo->prepare($sql);
         $statement->execute(array(
-            ':userid' => $userID;
+            ':userid' => $userID
         ));
 
         $sub_details = $statement->fetch(PDO::FETCH_ASSOC);
@@ -65,7 +65,7 @@ class database{
         end($sub_details);
         $last_key = key($sub_details);
 
-        return sub_details[$last_key];
+        return $sub_details[$last_key];
         
     }
     
